2b91062922fa508695d0744c5bce9655256f8483
Olfa <maslah.olfa@gmail.com>
2019-03-17 20:54:40 -0400

    Updates

---
+++ precache-manifest.fa78b3191a92d484e14ba0f4e5d9eb16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac7b1acc7c0ddc3fbc597d8a8b6077c8",
+    "url": "/BoxOfficeBoffo/static/media/boffoLogo.ac7b1acc.svg"
+  },
+  {
+    "revision": "d9322344311331447f25",
+    "url": "/BoxOfficeBoffo/static/js/runtime~main.d9322344.js"
+  },
+  {
+    "revision": "8f17cba514621cfd100c",
+    "url": "/BoxOfficeBoffo/static/js/main.8f17cba5.chunk.js"
+  },
+  {
+    "revision": "962260fbfe599e1af87f",
+    "url": "/BoxOfficeBoffo/static/js/2.962260fb.chunk.js"
+  },
+  {
+    "revision": "8f17cba514621cfd100c",
+    "url": "/BoxOfficeBoffo/static/css/main.445b5b47.chunk.css"
+  },
+  {
+    "revision": "012481b7d55ea380151fe82f9792af7a",
+    "url": "/BoxOfficeBoffo/index.html"
+  }
+];
